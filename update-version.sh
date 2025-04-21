6d9ee1306300c17aeff6962702ba87162033e87d
GGORG <GGORG0@protonmail.com>
2025-04-22 01:09:15 +0200

    added automatic version extractor

---
+++ update-version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CHANGELOG="src/proto-src/Changelog"
+CARGO_TOML="Cargo.toml"
+PROTO_SRC="src/proto-src"
+
+latest_major_minor=$(grep -oP '^## \[\K[0-9]+\.[0-9]+' "$CHANGELOG" | head -n1)
+semver_version="${latest_major_minor}.0"
+
+commit_hash=$(git -C "$PROTO_SRC" rev-parse --short HEAD)
+
+new_version="${semver_version}+${commit_hash}"
+
+sed -i -E "s/^version = \".*\"/version = \"${new_version}\"/" "$CARGO_TOML"
+
+echo "Updated Cargo.toml to version = \"$new_version\""
