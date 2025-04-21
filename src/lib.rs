f5e5e83f03191a470eb1dea855b6188366de7931
GGORG <GGORG0@protonmail.com>
2025-04-22 01:01:04 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+//! These are raw, automatically generated bindings to the official Flipper Zero protobuf protocol definitions from <https://github.com/flipperdevices/flipperzero-protobuf>.
+
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
