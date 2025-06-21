d973405bc731e1b34607e6264fa02100faf0570b
GGORG <GGORG0@protonmail.com>
2025-06-21 12:09:16 +0200

    protobuf_raw: re-export all used crates

--- flipperzero_protobuf_raw/src/lib.rs
+++ flipperzero_protobuf_raw/src/lib.rs
@@ -1,3 +1,8 @@
 //! These are raw, automatically generated bindings to the official Flipper Zero protobuf protocol definitions from <https://github.com/flipperdevices/flipperzero-protobuf>.
 
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+pub use pbjson;
+pub use pbjson_types;
+pub use prost;
+pub use serde;
